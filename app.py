60e96ba3256015f398fd67049e245fe71034d6d3
Brenofaria <brenofariac@gmail.com>
2022-06-11 18:35:16 -0400

    added postgresql database connection and functionality

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
+import psycopg2
+import os
 
 app = Flask(__name__)
+
+def connect_to_db():
+    connection = psycopg2.connect(
+        host="localhost",
+        database="games_project",
+        user=os.environ['DB_USERNAME'],
+        password=os.environ['DB_PASSWORD'],
+    )
+    return connection
 
 @app.route("/")
 def home():
@@ -10,6 +21,21 @@ def home():
 def mathgame():
     return render_template("mathgame.html")
 
-@app.route("/mathgameover")
+@app.route("/mathgameover", methods=('GET', 'POST'))
 def mathgameover():
+    if request.method == 'POST':
+        name = request.form['name']
+        score = request.form['score']
+
+        connection = connect_to_db()
+        cursor = connection.cursor()
+        cursor.execute(
+            'INSERT INTO leaderboard (name, score)'
+            'VALUES (%s, %s)',
+            (name, score)
+        )
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return redirect(url_for('home'))
     return render_template("mathgameover.html")
