c62416c7bec91e44acc33aa91114d5180ff18f50
Brenofaria <brenofariac@gmail.com>
2022-06-10 20:40:44 -0400

    game over redirect improved

--- static/scripts/mathgame.js
+++ static/scripts/mathgame.js
@@ -1,1 +1,10 @@
-
+function check(url)
+{
+    setInterval(() => {
+        let today = new Date();
+        if (today.getMinutes() === eval(localStorage.getItem("currMin")) && today.getSeconds() === eval(localStorage.getItem("currSec")) + 5)
+        {
+            window.location.href=url
+        }
+    }, 1000)
+}
